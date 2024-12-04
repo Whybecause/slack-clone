8167e39596f98688ccc94a05e6e605f655522908
whybecause <maxence.traina@gmail.com>
2024-12-04 22:48:00 +0100

    fix

--- src/components/convex-client-provider.tsx
+++ src/components/convex-client-provider.tsx
@@ -4,9 +4,7 @@ import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-const convexUrl = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_CONVEX_DEPLOYMENT_URL : process.env.NEXT_PUBLIC_CONVEX_URL
-
-const convex = new ConvexReactClient(convexUrl!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
